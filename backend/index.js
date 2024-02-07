42362c19e9d114452679cf94af9d64405cdbad88
Mishaelx6 <110745915+Mishaelx6@users.noreply.github.com>
2024-02-07 23:24:53 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,56 @@
-const dotenv = require('dotenv').config()
-const path = require('path')
-const express = require('express')
-const colors = require('colors')
-const cors = require('cors')
-const connectDB = require('./config/db')
-const { errorHandler } = require('./middleware/errorHandler')
-const fileUpload = require('express-fileupload')
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const { errorHandler } = require('./middleware/errorHandler');
+const fileUpload = require('express-fileupload');
 
+// Load environment variables
+dotenv;
 
-connectDB()
+// Connect to database
+connectDB();
 
-const app = express()
+// Create Express app
+const app = express();
+
+// Configure CORS
 app.use(cors({
   origin: ['https://fintech-bank-psi.vercel.app'], // Specify allowed origins
   methods: ['GET', 'POST'], // Specify allowed HTTP methods
   credentials: true // Allow credentials in cross-origin requests
 }));
-app.options('*', cors())
-// Enable CORS
-app.use(
-  fileUpload({
-    useTempFiles: true,
-  })
-)
-app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
-const PORT = process.env.PORT || 8080
+// Enable CORS preflight options
+app.options('*', cors());
 
-app.use(errorHandler)
-app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/', require('./routes/transactionRoutes'))
-app.use('/api/', require('./routes/requestRoutes'))
-app.use('/api/', require('./routes/uploadRoutes'))
+// File upload middleware
+app.use(fileUpload({
+  useTempFiles: true,
+}));
 
+// JSON and URL-encoded body parsing middleware
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// Define routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/', require('./routes/transactionRoutes'));
+app.use('/api/', require('./routes/requestRoutes'));
+app.use('/api/', require('./routes/uploadRoutes'));
+
+// Default route
 app.get('/', (req, res) => {
-  res.send('api is running...')
-})
+  res.send('API is running...');
+});
 
+// Start the server
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
   console.log(
     `Server Running on Port: http://localhost:${PORT} at ${new Date().toLocaleString(
       'en-US'
-    )}`.bgCyan.bold.underline
+    )}`
   )
-)
+);
+
