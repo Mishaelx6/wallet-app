995d934d6175dbd81058173a08331132ee16eb08
Mishaelx6 <110745915+Mishaelx6@users.noreply.github.com>
2024-02-08 12:57:40 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
-const dbUrl = process.env.DB_URL;
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-     const conn = await mongoose.connect(dbUrl)
+    const conn = await mongoose.connect("mongodb+srv://raisan:raisan123@cluster0.dclozxw.mongodb.net/wallet-app");
 
-    console.log(`MongoDB connected: ${conn.connection.host}`.bgGreen.black.bold.underline)
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
-    console.log(`Error: ${err.message}`.bgRed.bold.underline)
-    process.exit(1)
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
+
