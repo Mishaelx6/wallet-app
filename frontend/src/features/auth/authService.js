46cf36abe30c26c27474c8f1f314dfdbf893b195
Mishaelx6 <110745915+Mishaelx6@users.noreply.github.com>
2024-02-08 20:17:57 +0100

    Update authService.js

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,22 +1,19 @@
 import axios from 'axios'
 import { API_URL } from '../constants'
-// const API_URL = `${import.meta.env.VITE_URL}/api/users/`
-// const API_URL = 'http://localhost:8080/api/users/'
-console.log('FROM AUTH', API_URL)
 
 const login = async (userData) => {
-  const response = await axios.post(API_URL + '/api/users/login', userData)
+  const response = await axios.post(`${API_URL}/api/users/login`, userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
-     console.log('Token after login:', response.data.token); // Add this line
+    console.log('Token after login:', response.data.token);
   }
 
   return response.data
 }
 
 const register = async (userData) => {
-  const response = await axios.post(API_URL + '/api/users/register', userData)
+  const response = await axios.post(`${API_URL}/api/users/register`, userData)
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
   }
@@ -31,11 +28,11 @@ const getAllUsers = async (token) => {
       },
     };
 
-    const response = await axios.get(API_URL + '/api/users/get_users', config);
+    const response = await axios.get(`${API_URL}/api/users/get_users`, config);
 
     return response.data;
   } catch (error) {
-    throw error; // Rethrow the error
+    throw error;
   }
 }
 
