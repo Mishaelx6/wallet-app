4c5ef09acb7b4568cfe5f1561a5c3bd3b9e83449
Mishaelx6 <110745915+Mishaelx6@users.noreply.github.com>
2024-02-22 05:06:06 -0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://mishaeljohnson56:Jeremima6@cluster0.6ciru1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect('mongodb+srv://mishaeljohnson56:Jeremima6@cluster0.6ciru1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
